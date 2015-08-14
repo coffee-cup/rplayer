71ff944ef82300a7540565d8fbfae0b6a63f5b5e
Jake Runzer <jakerunzer@gmail.com>
2015-08-14 15:39:32 -0700

    track page views with ga iron router

--- rplayer.js
+++ rplayer.js
@@ -1,20 +1,8 @@
 if (Meteor.isClient) {
 
-// Template.loginForm.events({
-//     'submit form': function(event) {
-//       event.preventDefault();
-
-//       console.log(event.target.username.value + ' : ' + event.target.password.value);
-//       var username = event.target.username.value;
-//       var password = event.target.password.value;
-//       Meteor.loginWithReddit({username: username, password: password}, function(res, re) {
-//         console.log(res);
-//         console.log(re);
-//       });
-
-//       return false;
-//     }
-//   });
+  Router.configure({
+    trackPageView: true
+  });
 
 }
 
