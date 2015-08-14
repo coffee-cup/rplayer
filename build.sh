2c6f6e0bc5a8c0293fb4913b29a800de38f5937e
jakerunzer <jakerunzer@gmail.com>
2015-08-14 06:30:01 -0400

    add build and starter script

---
+++ build.sh
@@ -0,0 +1,7 @@
+rm -rf build/
+meteor build build
+cd build
+tar -zxf rplayer.tar.gz
+cd bundle/programs/server/
+npm install
+cd ../../../..
---
+++ starter.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        export ROOT_URL='http://rplayer.jakerunzer.me'
+        export PORT=7000
+        NODE_ENV=production forever start /var/www/rplayer/build/bundle/main.js >> /var/log/rplayer.txt 2>&1
+fi
