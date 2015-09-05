c4a1ce3e7f7ccdce47c72e8ca7890783009aaf73
Jake Runzer <jakerunzer@gmail.com>
2015-09-05 02:57:13 -0700

    update source vars

--- config/production/env.sh
+++ config/production/env.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export PATH=/usr/local/bin:$PATH
-export ROOT_URL='http://rplayer.jakerunzer.me'
+export ROOT_URL='http://boomz.xyz'
 export PORT=7000
 export METEOR_SETTINGS="$(cat config/settings.json)"
