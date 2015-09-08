49b750f26a11755c92bb64ef8dd830ca4eee88df
Jake Runzer <jakerunzer@gmail.com>
2015-09-07 17:17:06 -0700

    animate logo with gsap

---
+++ client/logo.js
@@ -0,0 +1,28 @@
+var EASE_DURATION = 0.6;
+var EASE_FUNCTION = Power2.easeInOut;
+
+var tl;
+var pause_next = false;
+
+
+Template.logo.onRendered(function() {
+  var songs = $('.song');
+  TweenLite.to(songs, 2, {width: "10px"});
+  tl = new TimelineMax({paused:true, repeat:-1});
+  tl.to('#logo-circle', EASE_DURATION, {scale:0.2, transformOrigin:"center center", ease:EASE_FUNCTION})
+    .to('#logo-circle', EASE_DURATION, {scale:1, transformOrigin:"center center", ease:EASE_FUNCTION});
+
+  tl.eventCallback("onRepeat", function() {
+    if (pause_next) {
+      pause_next = false;
+      tl.pause();
+    }
+  });
+
+  $('#logo-link-svg').on('mouseenter', function() {
+    tl.play();
+  }).on('mouseleave', function() {
+    pause_next = true;
+  });
+
+});
