1b3a75d4022813f0daa87ef67f253001c27e206c
Jake Runzer <jakerunzer@gmail.com>
2015-09-18 08:27:36 -0700

    need to export mongo url

--- config/production/env.sh
+++ config/production/env.sh
@@ -4,3 +4,4 @@ export PATH=/usr/local/bin:$PATH
 export ROOT_URL='http://boomz.xyz'
 export PORT=7000
 export METEOR_SETTINGS="$(cat config/settings.json)"
+export MONGO_URL='mongodb://localhost:27017/rplayer'
