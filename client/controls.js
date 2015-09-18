a4944515198ec59c6d9e6f8d428f1fcf118b0bb3
Jake Runzer <jakerunzer@gmail.com>
2015-09-17 21:32:24 -0700

    keybindings added

---
+++ client/controls.js
@@ -0,0 +1,25 @@
+Template.controls.helpers({
+  playing: function() {
+    return Session.get('playing');
+  },
+
+  canPlay: function() {
+    return Session.get('canPlay');
+  },
+
+  canPrev: function() {
+    return Session.get('canPrev');
+  },
+
+  canNext: function() {
+    return Session.get('canNext');
+  },
+
+  canPause: function() {
+    return Session.get('canPause');
+  },
+
+  canEye: function() {
+    return Session.get('canEye');
+  }
+});
